33d8381eb07544b5ccd99e09e7ec45064244a0be
animeshhh <animeshofficial9@gmail.com>
2023-07-10 19:45:47 +0530

    TargetArray.java added

---
+++ LeetCode-Solutions/Arrays/TargetArray.java
@@ -0,0 +1,22 @@
+package com.animesh;
+
+public class TargetArray {
+    public static void main(String[] args) {
+        int nums[] = new int[5];
+        nums[0] = 0;
+        nums[1] = 1;
+        nums[2] = 2;
+        nums[3] = 3;
+        nums[4] = 4;
+
+        int index[] = new int[5];
+        index[0] = 0;
+        index[1] = 1;
+        index[2] = 2;
+        index[3] = 2;
+        index[4] = 1;
+        int x = nums.length;
+
+    }
+
+}
